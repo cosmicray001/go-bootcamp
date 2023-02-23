087f1acedede7e9cb69d2a00f97b365b693bd298
cosmicray001 <samiulislambracu@gmail.com>
2023-02-24 02:13:03 +0600

    add: package alias example

--- books/An-Introduction-to-Programming-in-Go/chapter11/main.go
+++ books/An-Introduction-to-Programming-in-Go/chapter11/main.go
@@ -1,11 +1,11 @@
 package main
 
 import "fmt"
-import "github.com/cosmicray001/go-bootcamp/books/An-Introduction-to-Programming-in-Go/chapter11/math"
+import m "github.com/cosmicray001/go-bootcamp/books/An-Introduction-to-Programming-in-Go/chapter11/math"
 
 func main() {
 	nums := []float64{1, 2, 3, 4, 5, 6}
-	avg := math.Avg(nums)
+	avg := m.Avg(nums)
 
 	fmt.Println(avg)
 }
