4268ff3427c1792eff3a572f27c294172fe36f4a
cosmicray001 <samiulislambracu@gmail.com>
2023-03-01 02:39:44 +0600

    add: refactor code

--- book-app/main.go
+++ book-app/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
-  ID string `json:"id"`
-  Title string `json:"title"`
-  Author string `json:"author"`
-  Quantity int `json:"quantity"`
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
 }
 
 var books = []Book{
-  {"1", "In Search of Lost Time", "Marcel Proust", 1},
-  {"2", "The Great Gatsby", "F. Scott", 2},
-  {"3", "War and Peace", "Leo Tolstoy", 3},
+	{"1", "In Search of Lost Time", "Marcel Proust", 1},
+	{"2", "The Great Gatsby", "F. Scott", 2},
+	{"3", "War and Peace", "Leo Tolstoy", 3},
 }
 
-func getBooks(ctx *gin.Context) {
-  ctx.IndentedJSON(http.StatusOK, books)
+func bookList(ctx *gin.Context) {
+	ctx.IndentedJSON(http.StatusOK, books)
 }
 
-func createBook(ctx *gin.Context) {
-  var book Book
-  if err := ctx.BindJSON(&book); err != nil {
-    return
-  }
-  books = append(books, book)
-  ctx.IndentedJSON(http.StatusCreated, book)
+func bookCreate(ctx *gin.Context) {
+	var book Book
+	if err := ctx.BindJSON(&book); err != nil {
+		return
+	}
+	books = append(books, book)
+	ctx.IndentedJSON(http.StatusCreated, book)
 }
 
 func main() {
-  r := gin.Default()
-  r.GET("/books", getBooks)
-  r.POST("/book", createBook)
-  r.Run("127.0.0.1:8000")
+	r := gin.Default()
+	r.GET("/books", bookList)
+	r.POST("/book", bookCreate)
+	r.Run("127.0.0.1:8000")
 }
