263e9d53a66bf16dd78a4dc1ee1fd24c2f3302ce
cosmicray001 <samiulislambracu@gmail.com>
2023-03-02 23:48:49 +0600

    service file add

--- book-app/services/book_service.go
+++ book-app/services/book_service.go
@@ -2,10 +2,12 @@ package services
 
 import (
 	"github.com/cosmicray001/book-app/models"
+	"github.com/cosmicray001/book-app"
 	"errors"
 )
 
 func getBookById(id string) (*models.Book, error) {
+	books := main.GetDb()
 	for idx, book := range books {
 		if book.ID == id {
 			return &books[idx], nil
