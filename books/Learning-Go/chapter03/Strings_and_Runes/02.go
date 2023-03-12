dd018fc2206ebdab8b890a9a3250f4c47fc2e001
cosmicray001 <samiulislambracu@gmail.com>
2023-03-12 22:49:12 +0600

    add: complex string

---
+++ books/Learning-Go/chapter03/Strings_and_Runes/02.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "Hello 👻"
+	fmt.Println(len(s))
+	fmt.Println(s[0])
+	// for i := 0; i < len(s); i++ {
+	// 	fmt.Println(s[i])
+	// }
+}
